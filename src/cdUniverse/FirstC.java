572af1b6a1e4c708be65c5eae87b39980c38d7c4
thivyanWhy <thivyan.2014257@iit.ac.lk>
2015-03-13 22:52:31 +0530

    Working on both machines
    I have deleted the second class and i have added a print statement.

--- src/cdUniverse/FirstC.java
+++ src/cdUniverse/FirstC.java
@@ -6,6 +6,8 @@ public class FirstC {
 	public static void main(String [] args){
 		
 		System.out.println("Welcome to CD Universe!");
+		System.out.println("Hopefully i won't fail this!");
+
 		
 	}
 
